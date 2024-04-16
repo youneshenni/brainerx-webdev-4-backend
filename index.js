9350e6f37dccfc70721e327163eb92dfeee94879
Younes HENNI <youn.henni@gmail.com>
2024-04-16 19:41:02 +0100

    Routes handler

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const http = require('http');
 
 
 http.createServer(function (request, response) {
-    if (request.url === '/about') {
-        response.write('Welcome to the about page');
-    } else if (request.url === '/contact') {
-        response.write('Welcome to the contact page');
+    const routes = [
+        {
+            path: '/', handlerFunction: function (request, response) {
+                response.writeHead(200, { 'Content-Type': 'text/plain' });
+                response.end('Welcome to the homepage!');
+            }
+        },
+        {
+            path: '/about', handlerFunction: function (request, response) {
+                response.writeHead(200, { 'Content-Type': 'text/plain' });
+                response.end('Welcome to the about page!');
+            }
+        },
+        {
+            path: '/contact', handlerFunction: function (request, response) {
+                response.writeHead(200, { 'Content-Type': 'text/plain' });
+                response.end('Welcome to the contact page!');
+            }
+        },
+    ]
+
+    const path = request.url;
+    const chosenRoute = routes.find(function (route) { return (route.path === path) });
+    if (chosenRoute) {
+        chosenRoute.handlerFunction(request, response);
     } else {
-        response.writeHead(404);
-        response.write('404 Not Found!');
+        response.writeHead(404, { 'Content-Type': 'text/plain' });
+        response.end('404 Not Found');
     }
-    response.end();
 }).listen(8000);
