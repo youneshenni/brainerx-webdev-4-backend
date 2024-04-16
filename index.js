43fdf9d2aa7557d28fd86a19492281e9bb30ade8
Younes HENNI <youn.henni@gmail.com>
2024-04-16 19:21:34 +0100

    basic routing

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-// Initialize HTTP server with Node.js
-// Import required modules
 const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
 
-// Create HTTP server and listen on port 8000 for requests
+
 http.createServer(function (request, response) {
-    response.write('Hello World!'); // Write a response to the client
-    response.end(); // End the response
+    if (request.url === '/about') {
+        response.write('Welcome to the about page');
+    } else if (request.url === '/contact') {
+        response.write('Welcome to the contact page');
+    } else {
+        response.writeHead(404);
+        response.write('404 Not Found!');
+    }
+    response.end();
 }).listen(8000);
