b27c7765f66b50fd29eaa118b071522576f2445a
younes <youn.henni@icloud.com>
2024-04-17 20:08:47 +0100

    Adds Express.js

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-const http = require('http');
+const express = require("express");
+const { request } = require("http");
 
+const app = express();
 
-http.createServer(function (request, response) {
-    const routes = [
-        {
-            path: '/', handlerFunction: function (request, response) {
-                response.writeHead(200, { 'Content-Type': 'text/plain' });
-                response.end('Welcome to the homepage!');
-            }
-        },
-        {
-            path: '/about', handlerFunction: function (request, response) {
-                response.writeHead(200, { 'Content-Type': 'text/plain' });
-                response.end('Welcome to the about page!');
-            }
-        },
-        {
-            path: '/contact', handlerFunction: function (request, response) {
-                response.writeHead(200, { 'Content-Type': 'text/plain' });
-                response.end('Welcome to the contact page!');
-            }
-        },
-    ]
+const list = ["item1", "item2", "item3", "item4"];
 
-    const path = request.url;
-    const chosenRoute = routes.find(function (route) { return (route.path === path) });
-    if (chosenRoute) {
-        chosenRoute.handlerFunction(request, response);
-    } else {
-        response.writeHead(404, { 'Content-Type': 'text/plain' });
-        response.end('404 Not Found');
-    }
-}).listen(8000);
+app.get("/", (_, res) =>
+  res.send(`<ul>${list.map((item) => `<li>${item}</li>`).join("")}</ul>`)
+);
+
+app.listen(8000);
