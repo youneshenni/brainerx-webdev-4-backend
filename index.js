1d7283d4097c3483f7c3344fe1f20e8e7041e152
younes <youn.henni@icloud.com>
2024-04-17 20:26:33 +0100

    EJS

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { request } = require("http");
+let ejs = require("ejs");
 
 const app = express();
 
 const list = ["item1", "item2", "item3", "item4"];
 
-app.get("/", (_, res) =>
-  res.send(`<ul>${list.map((item) => `<li>${item}</li>`).join("")}</ul>`)
-);
+app.set("view engine", "ejs");
+
+app.get("/", (_, res) => res.render("list", { list }));
 
 app.listen(8000);
