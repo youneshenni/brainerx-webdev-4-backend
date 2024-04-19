2e74a578d379a367f2951671248c6c0109c5c6c8
younes <youn.henni@icloud.com>
2024-04-19 19:06:44 +0100

    Table display

--- index.js
+++ index.js
@@ -3,16 +3,32 @@ let ejs = require("ejs");
 
 const app = express();
 
-const list = ["item1", "item2", "item3", "item4"];
+const users = [
+  {
+    name: "John",
+    age: 25,
+    email: "john@example.com",
+  },
+  {
+    name: "Jane",
+    age: 30,
+    email: "jane@gmail.com",
+  },
+  {
+    name: "Doe",
+    age: 35,
+    email: "doe@example.com",
+  },
+];
 
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-app.get("/", (_, res) => res.render("list", { list }));
+app.get("/", (_, res) => res.render("list", { users }));
 app.post("/", (req, res) => {
-  list.push(req.body.text);
-  res.render("list", { list });
+  //   list.push(req.body.text);
+  res.render("users", { users });
 });
 
 app.listen(8000);
