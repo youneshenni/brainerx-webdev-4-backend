d061a57e8a65cda1d6ee22fe3f817a6e5d91752c
younes <youn.henni@icloud.com>
2024-04-19 18:19:03 +0100

    adds insertion

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const app = express();
 
 const list = ["item1", "item2", "item3", "item4"];
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 app.get("/", (_, res) => res.render("list", { list }));
+app.post("/", (req, res) => {
+  list.push(req.body.text);
+  res.render("list", { list });
+});
 
 app.listen(8000);
