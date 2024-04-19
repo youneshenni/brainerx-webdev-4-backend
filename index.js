035634b09cdba9b9c57d0d2026b67b285899d43d
younes <youn.henni@icloud.com>
2024-04-19 20:44:17 +0100

    adds data verification

--- index.js
+++ index.js
@@ -25,10 +25,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-app.get("/", (_, res) => res.render("list", { users }));
+app.get("/", (_, res) => res.render("list", { users, error: "" }));
 app.post("/", (req, res) => {
-  //   list.push(req.body.text);
-  res.render("users", { users });
+  let error = null;
+  if (!req.body.name) error = { message: "Name is required" };
+  if (!req.body.age) error = { message: "Age is required" };
+  if (!req.body.email) error = { message: "Email is required" };
+  if (req.body.age < 17) error = { message: "Age must be at least 18" };
+  if (req.body.age > 200) error = { message: "Age must be less than 200" };
+  if (/^[a-zA-Z\.]+\@[a-zA-Z]+(\.[a-zA-Z]+)+$/.test(req.body.email) === false)
+    error = { message: "Invalid email" };
+  if (!error) users.push(req.body);
+  res.render("list", { users, error: error?.message || "" });
 });
 
 app.listen(8000);
