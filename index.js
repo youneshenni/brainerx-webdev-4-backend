a1f84789c2681c084f6b67ebef36ce1c98d9a7cd
younes <youn.henni@icloud.com>
2024-04-22 19:28:55 +0100

    adds database persistence with sqlite

--- index.js
+++ index.js
@@ -2,40 +2,59 @@ const express = require("express");
 const { readFileSync, writeFileSync, existsSync } = require("fs");
 const usersPath = "./users.json";
 
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./database.sqlite",
+});
+const User = sequelize.define(
+  "User",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ["email"],
+      },
+    ],
+  }
+);
+
+sequelize.sync({ alter: true });
+
 const app = express();
 
-function initializeUsersFile() {
-  writeFileSync(usersPath, "[]");
-}
+const getUsers = () => User.findAll();
 
-function readUsersFromFile() {
-  if (!existsSync(usersPath)) {
-    initializeUsersFile();
-    return [];
-  }
-  const data = readFileSync(usersPath);
-  try {
-    return JSON.parse(data);
-  } catch (e) {
-    initializeUsersFile();
-    return [];
-  }
-}
-
-function writeToFile(data) {
-  writeFileSync(usersPath, JSON.stringify(data, null, 2));
-}
+const writeUser = (user) => User.create(user);
 
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-app.get("/", (_, res) => {
-  const users = readUsersFromFile();
+app.get("/", async (_, res) => {
+  const users = await getUsers();
   res.render("list", { users, error: "" });
 });
-app.post("/", (req, res) => {
-  const users = readUsersFromFile();
+app.post("/", async (req, res) => {
   let error = null;
   if (!req.body.name) error = { message: "Name is required" };
   if (!req.body.age) error = { message: "Age is required" };
@@ -45,9 +64,17 @@ app.post("/", (req, res) => {
   if (/^[a-zA-Z\.]+\@[a-zA-Z]+(\.[a-zA-Z]+)+$/.test(req.body.email) === false)
     error = { message: "Invalid email" };
   if (!error) {
-    users.push(req.body);
-    writeToFile(users);
+    try {
+      await writeUser(req.body);
+    } catch (e) {
+      if (e.errors[0].message === "email must be unique") {
+        error = { message: "Email already exists" };
+      } else {
+        error = { message: "Unknown error" };
+      }
+    }
   }
+  const users = await getUsers();
   res.render("list", { users, error: error?.message || "" });
 });
 
