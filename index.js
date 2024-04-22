59f0e4a993bc4eef50f1800e68f4c69bc7a5ac58
younes <youn.henni@icloud.com>
2024-04-22 21:31:09 +0100

    adds jwt login

--- index.js
+++ index.js
@@ -1,42 +1,24 @@
 const express = require("express");
-const { readFileSync, writeFileSync, existsSync } = require("fs");
-const usersPath = "./users.json";
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const { Sequelize, DataTypes } = require("sequelize");
+const cookieParser = require("cookie-parser");
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./database.sqlite",
 });
-const User = sequelize.define(
-  "User",
-  {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    age: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+const User = sequelize.define("User", {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
-  {
-    indexes: [
-      {
-        unique: true,
-        fields: ["email"],
-      },
-    ],
-  }
-);
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
 sequelize.sync({ alter: true });
 
@@ -50,10 +32,26 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-app.get("/", async (_, res) => {
-  const users = await getUsers();
-  res.render("list", { users, error: "" });
+app.use(cookieParser());
+
+app.get("/", async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) return res.redirect("/login");
+  jwt.verify(token, "brainerx", async (err, decoded) => {
+    if (err) return res.redirect("/login");
+    const users = await User.findAll();
+    res.render("list", { users, error: "" });
+  });
 });
+
+app.get("/login", (_, res) => {
+  res.render("login", { error: "" });
+});
+
+app.get("/register", (_, res) => {
+  res.render("register", { error: "" });
+});
+
 app.post("/", async (req, res) => {
   let error = null;
   if (!req.body.name) error = { message: "Name is required" };
@@ -76,6 +74,50 @@ app.post("/", async (req, res) => {
   }
   const users = await getUsers();
   res.render("list", { users, error: error?.message || "" });
+});
+
+app.post("/register", async (req, res) => {
+  let error = null;
+  if (!req.body.username) error = { message: "Name is required" };
+  if (!req.body.password) error = { message: "Password is required" };
+  if (!error) {
+    try {
+      await User.create({
+        ...req.body,
+        password: bcrypt.hashSync(req.body.password, 10),
+      });
+      res.redirect("/login");
+    } catch (e) {
+      error = { message: e.message };
+    }
+  }
+  res.render("register", { error: error?.message || "" });
+});
+
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+
+  console.log(username);
+  const foundUser = await User.findOne({
+    where: {
+      username: username,
+    },
+  });
+
+  if (!foundUser) return res.status(404).send("User not found");
+
+  const passwordMatch = bcrypt.compareSync(
+    req.body.password,
+    foundUser.password
+  );
+
+  if (passwordMatch) {
+    const token = jwt.sign({ username: foundUser.username }, "brainerx");
+    res.cookie("token", token);
+    res.redirect("/");
+  } else {
+    res.send("Wrong password");
+  }
 });
 
 app.listen(8000);
