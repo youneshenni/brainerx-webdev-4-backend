2f2f06ed4c1ae1ce81ed71cbe49f39bd00700fd4
younes <youn.henni@icloud.com>
2024-04-24 20:14:29 +0100

    promise / async await

--- promise/index.js
+++ promise/index.js
@@ -1,4 +1,5 @@
-const { existsSync, mkdirSync, writeFileSync, mkdir } = require("fs");
+const { mkdir, stat, writeFile } = require("fs/promises");
+const { join } = require("path");
 
 const files = [
   "folder1/folder2/folder3/file.txt",
@@ -6,15 +7,16 @@ const files = [
   "folder1/folder1/folder6/file.txt",
 ];
 
-for (let i = 0; i < files.length; i++) {
-  const currentPath = files[i];
-  const folders = currentPath.split("/").slice(0, -1);
-  let path = "";
-  for (let j = 0; j < folders.length; j++) {
-    path += folders[j] + "/";
-    if (!existsSync(path)) {
-      mkdirSync(path);
-    }
-  }
-  writeFileSync(currentPath, "");
-}
+const createRecursiveFolders = async (path, acc = "") => {
+  await stat(join(acc, path[0])).catch((err) => {
+    if (err.code === "ENOENT") return mkdir(join(acc, path[0]));
+  });
+  if (path.length > 1)
+    return createRecursiveFolders(path.slice(1), join(acc, path[0]));
+};
+
+Promise.all(
+  files
+    .map((file) => file.split("/").slice(0, -1))
+    .map((result) => createRecursiveFolders(result))
+).then(files.map((file) => writeFile(file, "Hello world!")));
