7f929cadc64e206045935abac3e49de80e47e8bc
younes <youn.henni@icloud.com>
2024-04-28 18:44:43 +0100

    Converts to API: Login / users

--- index.js
+++ index.js
@@ -30,46 +30,38 @@ const writeUser = (user) => User.create(user);
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.json());
+
 app.set("view engine", "ejs");
 
 app.use(cookieParser());
 
 app.get("/", async (req, res) => {
-  const token = req.cookies.token;
-  if (!token) return res.redirect("/login");
-  jwt.verify(token, "brainerx", async (err, decoded) => {
-    if (err) return res.redirect("/login");
-    const users = await User.findAll();
-    res.render("list", { users, error: "" });
-  });
+  res.render("list");
 });
 
 app.get("/login", (_, res) => {
-  res.render("login", { error: "" });
+  res.render("login");
 });
 
 app.get("/register", (_, res) => {
-  res.render("register", { error: "" });
+  res.render("register");
 });
 
 app.post("/", async (req, res) => {
-  let error = null;
-  if (!req.body.username) error = { message: "Name is required" };
-  if (!req.body.password) error = { message: "Password is required" };
-  if (!error) {
-    try {
-      await writeUser(req.body);
-    } catch (e) {
-      if (e.errors[0].message === "email must be unique") {
-        error = { message: "Email already exists" };
-      } else {
-        console.error(error);
-        error = { message: "Unknown error" };
-      }
+  if (!req.body.username) return res.status(400).send("username is required");
+  if (!req.body.password) return res.status(400).send("Password is required");
+  try {
+    await writeUser(req.body);
+    return res.status(201).send("Created");
+  } catch (e) {
+    if (e.errors[0].message === "email must be unique") {
+      return res.status(409).send("User already exists");
+    } else {
+      console.error(error);
+      return res.status(500).send("Internal Server Error");
     }
   }
-  const users = await getUsers();
-  res.render("list", { users, error: error?.message || "" });
 });
 
 app.post("/register", async (req, res) => {
@@ -110,7 +102,7 @@ app.post("/login", async (req, res) => {
   if (passwordMatch) {
     const token = jwt.sign({ username: foundUser.username }, "brainerx");
     res.cookie("token", token);
-    res.redirect("/");
+    res.status(201).send("Created");
   } else {
     res.send("Wrong password");
   }
